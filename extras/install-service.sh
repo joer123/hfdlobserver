f42259f3dcebe47ea7efbd772e5bf6274c098c99
Kuupa Ork <kuupaork+github@hfdl.observer>
2025-01-07 20:34:00 -0500

    fixes for systemd service installer. May possibly work?

--- extras/install-service.sh
+++ extras/install-service.sh
@@ -5,8 +5,9 @@
 # TL;DR: BSD 3-clause
 #
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-sudo cp "${SCRIPT_DIR}/etc-default-hfdlobserver888" /etc/default/hfdlobserver888
-sed -e "s:[$]USER:${USER}:g" -s":[$]HOME:${HOME}:g"< "${SCRIPT_DIR}/hfdlobserver888.service" \
-| sudo tee /etc/systemd/system/hfdlobserver888.service
-sudo systemctl enable hfdlobserver888
-echo "Systemd unit installed and enabled. You can start it now with `sudo systemctl start hfdlobserver888`"
+sudo cp "${SCRIPT_DIR}/etc-default-hfdlobserver888" /etc/default/hfdlobserver888 \
+&& sed -e "s:[$]USER:${USER}:g" -e "s:[$]HOME:${HOME}:g" < "${SCRIPT_DIR}/hfdlobserver888.service" \
+| sudo tee /etc/systemd/system/hfdlobserver888.service \
+&& sudo systemctl daemon-reload \
+&& sudo systemctl enable hfdlobserver888 \
+&& echo "Systemd unit installed and enabled. You can start it now with `sudo systemctl start hfdlobserver888`"
